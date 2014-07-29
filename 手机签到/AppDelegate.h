3a083cf18e4574bb1b9cf6fa1881dfdd94de545f
jackylee <orange.jackylee@gmail.com>
2014-07-29 09:19:01 +0800

    Initial Commit

---
+++ 手机签到/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  手机签到
+//
+//  Created by fred on 14-7-29.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 手机签到/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  手机签到
+//
+//  Created by fred on 14-7-29.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
